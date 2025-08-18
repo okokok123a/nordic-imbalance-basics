bcfc7b59189ffcea2903876c12a6e9f2a44fb667
EmotionalTrader <sohail.mohseni@gmail.com>
2025-08-18 12:45:35 +0200

    test(api): assert out_Domain present in ENTSO-E dry-run URLs

--- tests/test_entsoe_dry_run.py
+++ tests/test_entsoe_dry_run.py
@@ -20,10 +20,14 @@ def test_se3_dry_run_url():
     assert "documentType=A44" in out
     assert "processType=A01" in out
     assert "in_Domain=10Y1001A1001A46L" in out
+    assert "out_Domain=10Y1001A1001A46L" in out
     assert "periodStart=202505100000" in out
     assert "periodEnd=202505130000" in out
 
 def test_se4_dry_run_url():
     out = _run_dry("SE4")
     assert "DRY-RUN URL:" in out
+    assert "documentType=A44" in out
+    assert "processType=A01" in out
     assert "in_Domain=10Y1001A1001A47J" in out
+    assert "out_Domain=10Y1001A1001A47J" in out
